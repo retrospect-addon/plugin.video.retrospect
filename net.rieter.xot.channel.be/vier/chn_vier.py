cf89bdf0737bc9071aa4abe15164741cc64e7aaa
Bas Rieter <basrieter@gmail.com>
2019-01-03 19:13:07 +0100

    PEP: updated Vier.be

--- net.rieter.xot.channel.be/vier/chn_vier.py
+++ net.rieter.xot.channel.be/vier/chn_vier.py
@@ -20,69 +20,67 @@ class Channel(chn_class.Channel):
     main class from which all channels inherit
     """
 
-    def __init__(self, channelInfo):
-        """Initialisation of the class.
-
-        Arguments:
-        channelInfo: ChannelInfo - The channel info object to base this channel on.
+    def __init__(self, channel_info):
+        """ Initialisation of the class.
 
         All class variables should be instantiated here and this method should not
         be overridden by any derived classes.
 
+        :param ChannelInfo channel_info: The channel info object to base this channel on.
+
         """
 
-        chn_class.Channel.__init__(self, channelInfo)
+        chn_class.Channel.__init__(self, channel_info)
 
         # setup the main parsing data
         if self.channelCode == "vijfbe":
             self.noImage = "vijfimage.png"
             self.mainListUri = "https://www.vijf.be/programmas"
             self.baseUrl = "https://www.vijf.be"
+
         # elif self.channelCode == "zesbe":
         #     self.noImage = "zesimage.png"
         #     self.mainListUri = "https://www.zestv.be/programmas"
         #     self.baseUrl = "https://www.zestv.be"
+
         else:
             self.noImage = "vierimage.png"
             self.mainListUri = "https://www.vier.be/programmas"
             self.baseUrl = "https://www.vier.be"
 
-        episodeRegex = '<a class="program-overview__link" href="(?<url>[^"]+)">(?<title>[^<]+)</a>'
-        episodeRegex = Regexer.from_expresso(episodeRegex)
+        episode_regex = '<a class="program-overview__link" href="(?<url>[^"]+)">(?<title>[^<]+)</a>'
+        episode_regex = Regexer.from_expresso(episode_regex)
         self._add_data_parser(self.mainListUri, match_type=ParserData.MatchExact,
-                              parser=episodeRegex,
+                              parser=episode_regex,
                               creator=self.create_episode_item)
 
-        videoRegex = '<a(?:[^>]+data-background-image="(?<thumburl>[^"]+)")?[^>]+href="(?<url>/video/[^"]+)"[^>]*>(?:\s+<div[^>]+>\s+<div [^>]+data-background-image="(?<thumburl2>[^"]+)")?[\w\W]{0,1000}?<h3[^>]*>(?:<span>)?(?<title>[^<]+)(?:</span>)?</h3>(?:\s+(?:<div[^>]*>\s+)?<div[^>]*>[^<]+</div>\s+<div[^>]+data-timestamp="(?<timestamp>\d+)")?'
-        videoRegex = Regexer.from_expresso(videoRegex)
+        video_regex = r'<a(?:[^>]+data-background-image="(?<thumburl>[^"]+)")?[^>]+href="' \
+                      r'(?<url>/video/[^"]+)"[^>]*>(?:\s+<div[^>]+>\s+<div [^>]+' \
+                      r'data-background-image="(?<thumburl2>[^"]+)")?[\w\W]{0,1000}?' \
+                      r'<h3[^>]*>(?:<span>)?(?<title>[^<]+)(?:</span>)?</h3>(?:\s+' \
+                      r'(?:<div[^>]*>\s+)?<div[^>]*>[^<]+</div>\s+<div[^>]+data-timestamp=' \
+                      r'"(?<timestamp>\d+)")?'
+        video_regex = Regexer.from_expresso(video_regex)
         self._add_data_parser("*", match_type=ParserData.MatchExact,
                               name="Normal video items",
-                              parser=videoRegex,
+                              parser=video_regex,
                               creator=self.create_video_item)
 
-        pageRegex = '<button class="button button--default js-load-more-button"\W+data-url="(?<url>[^"]+)"\W+data-page="(?<title>\d+)"'
-        pageRegex = Regexer.from_expresso(pageRegex)
+        page_regex = r'<button class="button button--default js-load-more-button"\W+data-url=' \
+                     r'"(?<url>[^"]+)"\W+data-page="(?<title>\d+)"'
+        page_regex = Regexer.from_expresso(page_regex)
         self._add_data_parser("*", match_type=ParserData.MatchExact,
-                              parser=pageRegex,
+                              parser=page_regex,
                               creator=self.create_page_item)
 
         self._add_data_parser("/api/program/fixed/", name="API paging",
                               match_type=ParserData.MatchContains,
-                              # json=False,
-                              preprocessor=self.ExtractPageData,
-                              parser=videoRegex,
+                              preprocessor=self.extract_page_data,
+                              parser=video_regex,
                               creator=self.create_video_item)
 
-        # imageVideoRegex = '<a[^>]+url\((?<thumburl>[^)]+)[^>]+href="(?<url>/video/[^"]+)"[\w\W]{500,2000}<h3[^>]+>(?<title>[^<]+)</h3>\W*<div[^>]*>(?<description>[^<]+)(?:</div>\W*<div[^>]*>\W*)?<div[^>]+data-videoid="(?<videoid>[^"]+)"'
-        # imageVideoRegex = Regexer.from_expresso(imageVideoRegex)
-        # self._add_data_parser("*", matchType=ParserData.MatchExact,
-        #                     parser=imageVideoRegex,
-        #                     creator=self.create_video_item)
-
         # Generic updater with login
-        self._add_data_parser("*",
-                              # requiresLogon=True,
-                              updater=self.update_video_item)
+        self._add_data_parser("*", updater=self.update_video_item)
 
         # ==========================================================================================
         # Channel specific stuff
@@ -97,18 +95,34 @@ class Channel(chn_class.Channel):
         return
 
     def log_on(self):
+        """ Logs on to a website, using an url.
+
+        First checks if the channel requires log on. If so and it's not already
+        logged on, it should handle the log on. That part should be implemented
+        by the specific channel.
+
+        More arguments can be passed on, but must be handled by custom code.
+
+        After a successful log on the self.loggedOn property is set to True and
+        True is returned.
+
+        :return: indication if the login was successful.
+        :rtype: bool
+
+        """
+
         if self.__idToken:
             return True
 
         # check if there is a refresh token
         # refresh token: viervijfzes_refresh_token
-        refreshToken = AddonSettings.get_setting("viervijfzes_refresh_token")
+        refresh_token = AddonSettings.get_setting("viervijfzes_refresh_token")
         client = AwsIdp("eu-west-1_dViSsKM5Y", "6s1h851s8uplco5h6mqh1jac8m",
                         proxy=self.proxy, logger=Logger.instance())
-        if refreshToken:
-            idToken = client.RenewToken(refreshToken)
-            if idToken:
-                self.__idToken = idToken
+        if refresh_token:
+            id_token = client.RenewToken(refresh_token)
+            if id_token:
+                self.__idToken = id_token
                 return True
             else:
                 Logger.info("Extending token for VierVijfZes failed.")
@@ -125,31 +139,30 @@ class Channel(chn_class.Channel):
             )
             return False
 
-        idToken, refreshToken = client.Authenticate(username, password)
-        if not idToken or not refreshToken:
+        id_token, refresh_token = client.Authenticate(username, password)
+        if not id_token or not refresh_token:
             Logger.error("Error getting a new token. Wrong password?")
             return False
 
-        self.__idToken = idToken
-        AddonSettings.set_setting("viervijfzes_refresh_token", refreshToken)
+        self.__idToken = id_token
+        AddonSettings.set_setting("viervijfzes_refresh_token", refresh_token)
         return True
 
-    def create_episode_item(self, resultSet):
-        """Creates a new MediaItem for an episode
-
-        Arguments:
-        resultSet : list[string] - the resultSet of the self.episodeItemRegex
-
-        Returns:
-        A new MediaItem of type 'folder'
+    def create_episode_item(self, result_set):
+        """ Creates a new MediaItem for an episode.
 
         This method creates a new MediaItem from the Regular Expression or Json
-        results <resultSet>. The method should be implemented by derived classes
+        results <result_set>. The method should be implemented by derived classes
         and are specific to the channel.
+
+        :param list[str]|dict[str,str] result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'folder'.
+        :rtype: MediaItem|none
 
         """
 
-        item = chn_class.Channel.create_episode_item(self, resultSet)
+        item = chn_class.Channel.create_episode_item(self, result_set)
         if item is None:
             return item
 
@@ -158,15 +171,44 @@ class Channel(chn_class.Channel):
         item.thumb = item.thumb or self.noImage
         return item
 
-    def create_page_item(self, resultSet):
-        resultSet["url"] = "{0}/{1}".format(resultSet["url"], resultSet["title"])
-        resultSet["title"] = str(int(resultSet["title"]) + 1)
+    def create_page_item(self, result_set):
+        """ Creates a MediaItem of type 'page' using the result_set from the regex.
 
-        item = self.create_folder_item(resultSet)
+        This method creates a new MediaItem from the Regular Expression or Json
+        results <result_set>. The method should be implemented by derived classes
+        and are specific to the channel.
+
+        :param list[str]|dict[str,str] result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'page'.
+        :rtype: MediaItem|none
+
+        """
+
+        result_set["url"] = "{0}/{1}".format(result_set["url"], result_set["title"])
+        result_set["title"] = str(int(result_set["title"]) + 1)
+
+        item = self.create_folder_item(result_set)
         item.type = "page"
         return item
 
-    def ExtractPageData(self, data):
+    def extract_page_data(self, data):
+        """ Performs pre-process actions for data processing.
+
+        Accepts an data from the process_folder_list method, BEFORE the items are
+        processed. Allows setting of parameters (like title etc) for the channel.
+        Inside this method the <data> could be changed and additional items can
+        be created.
+
+        The return values should always be instantiated in at least ("", []).
+
+        :param str data: The retrieve data that was loaded for the current item and URL.
+
+        :return: A tuple of the data and a list of MediaItems that were generated.
+        :rtype: tuple[str|JsonHelper,list[MediaItem]]
+
+        """
+
         items = []
         json = JsonHelper(data)
         data = json.get_value("data")
@@ -175,22 +217,16 @@ class Channel(chn_class.Channel):
         if json.get_value("loadMore", fallback=False):
             url, page = self.parentItem.url.rsplit("/", 1)
             url = "{0}/{1}".format(url, int(page) + 1)
-            pageItem = MediaItem("{0}".format(int(page) + 2), url)
-            pageItem.type = "page"
-            items.append(pageItem)
+            page_item = MediaItem("{0}".format(int(page) + 2), url)
+            page_item.type = "page"
+            items.append(page_item)
         return data, items
 
-    def create_video_item(self, resultSet):
-        """Creates a MediaItem of type 'video' using the resultSet from the regex.
-
-        Arguments:
-        resultSet : tuple (string) - the resultSet of the self.videoItemRegex
-
-        Returns:
-        A new MediaItem of type 'video' or 'audio' (despite the method's name)
+    def create_video_item(self, result_set):
+        """ Creates a MediaItem of type 'video' using the result_set from the regex.
 
         This method creates a new MediaItem from the Regular Expression or Json
-        results <resultSet>. The method should be implemented by derived classes
+        results <result_set>. The method should be implemented by derived classes
         and are specific to the channel.
 
         If the item is completely processed an no further data needs to be fetched
@@ -198,9 +234,14 @@ class Channel(chn_class.Channel):
         self.update_video_item method is called if the item is focussed or selected
         for playback.
 
+        :param list[str]|dict[str,str] result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'video' or 'audio' (despite the method's name).
+        :rtype: MediaItem|none
+
         """
 
-        item = chn_class.Channel.create_video_item(self, resultSet)
+        item = chn_class.Channel.create_video_item(self, result_set)
 
         # All of vier.be video's seem GEO locked.
         item.isGeoLocked = True
@@ -208,28 +249,22 @@ class Channel(chn_class.Channel):
         # Set the correct url
         # videoId = resultSet["videoid"]
         # item.url = "https://api.viervijfzes.be/content/%s" % (videoId, )
-        time_stamp = resultSet.get("timestamp")
+        time_stamp = result_set.get("timestamp")
         if time_stamp:
-            dateTime = DateHelper.get_date_from_posix(int(resultSet["timestamp"]))
-            item.set_date(dateTime.year, dateTime.month, dateTime.day, dateTime.hour,
-                          dateTime.minute,
-                          dateTime.second)
+            date_time = DateHelper.get_date_from_posix(int(result_set["timestamp"]))
+            item.set_date(date_time.year, date_time.month, date_time.day, date_time.hour,
+                          date_time.minute,
+                          date_time.second)
 
-        if not item.thumb and "thumburl2" in resultSet and resultSet["thumburl2"]:
-            item.thumb = resultSet["thumburl2"]
+        if not item.thumb and "thumburl2" in result_set and result_set["thumburl2"]:
+            item.thumb = result_set["thumburl2"]
 
         if item.thumb and item.thumb != self.noImage:
             item.thumb = HtmlEntityHelper.strip_amp(item.thumb)
         return item
 
     def update_video_item(self, item):
-        """Updates an existing MediaItem with more data.
-
-        Arguments:
-        item : MediaItem - the MediaItem that needs to be updated
-
-        Returns:
-        The original item with more data added to it's properties.
+        """ Updates an existing MediaItem with more data.
 
         Used to update none complete MediaItems (self.complete = False). This
         could include opening the item's URL to fetch more data and then process that
@@ -243,6 +278,11 @@ class Channel(chn_class.Channel):
         if the returned item does not have a MediaItemPart then the self.complete flag
         will automatically be set back to False.
 
+        :param MediaItem item: the original MediaItem that needs updating.
+
+        :return: The original item with more data added to it's properties.
+        :rtype: MediaItem
+
         """
 
         Logger.debug('Starting update_video_item for %s (%s)', item.name, self.channelName)
@@ -252,42 +292,41 @@ class Channel(chn_class.Channel):
         # url = "%s/video/v3/embed/%s" % (self.baseUrl, videoId,)
         url = item.url
         data = UriHandler.open(url, proxy=self.proxy)
-        return self.__UpdateVideo(item, data)
+        return self.__update_video(item, data)
 
-    def __UpdateVideo(self, item, data):
+    def __update_video(self, item, data):
         regex = 'data-file="([^"]+)'
-        m3u8Url = Regexer.do_regex(regex, data)[-1]
+        m3u8_url = Regexer.do_regex(regex, data)[-1]
 
-        if ".m3u8" not in m3u8Url:
+        if ".m3u8" not in m3u8_url:
             Logger.info("Not a direct M3u8 file. Need to log in")
-            url = "https://api.viervijfzes.be/content/%s" % (m3u8Url, )
+            url = "https://api.viervijfzes.be/content/%s" % (m3u8_url, )
 
             # We need to log in
             if not self.loggedOn:
                 self.log_on()
 
             # add authorization header
-            authenticationHeader = {
+            authentication_header = {
                 "authorization": self.__idToken,
                 "content-type": "application/json"
             }
-            data = UriHandler.open(url, proxy=self.proxy, additional_headers=authenticationHeader)
-            jsonData = JsonHelper(data)
-            m3u8Url = jsonData.get_value("video", "S")
+            data = UriHandler.open(url, proxy=self.proxy, additional_headers=authentication_header)
+            json_data = JsonHelper(data)
+            m3u8_url = json_data.get_value("video", "S")
 
         # Geo Locked?
-        if "geo" in m3u8Url.lower():
+        if "geo" in m3u8_url.lower():
             # set it for the error statistics
             item.isGeoLocked = True
 
         part = item.create_new_empty_media_part()
-        for s, b in M3u8.get_streams_from_m3u8(m3u8Url, self.proxy):
+        for s, b in M3u8.get_streams_from_m3u8(m3u8_url, self.proxy):
             if int(b) < 200:
                 Logger.info("Skipping stream of quality '%s' kbps", b)
                 continue
 
             item.complete = True
-            # s = self.get_verifiable_video_url(s)
             part.append_media_stream(s, b)
 
         item.complete = True
