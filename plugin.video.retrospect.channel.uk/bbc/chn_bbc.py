a21c3d2ac5a7b5fc1660aca0fbd4a764e5e8257d
Bas Rieter <basrieter@gmail.com>
2019-05-19 21:21:20 +0200

    Fixed: BBC did not work at all

--- plugin.video.retrospect.channel.uk/bbc/chn_bbc.py
+++ plugin.video.retrospect.channel.uk/bbc/chn_bbc.py
@@ -1,13 +1,15 @@
 import chn_class
+from helpers.languagehelper import LanguageHelper
 from mediaitem import MediaItem
 from helpers.xmlhelper import XmlHelper
 from helpers import subtitlehelper
 from helpers.jsonhelper import JsonHelper
-from helpers.datehelper import DateHelper
 from streams.f4m import F4m
 from logger import Logger
 from parserdata import ParserData
 from regexer import Regexer
+from streams.m3u8 import M3u8
+from streams.mpd import Mpd
 from urihandler import UriHandler
 
 
@@ -28,33 +30,47 @@ class Channel(chn_class.Channel):
         self.liveUrl = None
 
         # setup the urls
-        self.baseUrl = "http://www.bbc.co.uk/"
+        self.baseUrl = "http://www.bbc.co.uk"
         self.swfUrl = "http://emp.bbci.co.uk/emp/SMPf/1.13.13/StandardMediaPlayerChromelessFlash.swf"
 
         self.noImage = "bbciplayerimage.png"
-        self.mainListUri = "http://www.bbc.co.uk/iplayer"
+        self.mainListUri = "#mainlisting"
 
         # setup the main parsing data
         self.episodeItemRegex = '<a class="letter stat" href="(?<url>/iplayer/a-z/[^"]+)">(?<title>[^<]+)</a>'\
                                 .replace("(?<", "(?P<")
         self._add_data_parser(self.mainListUri, match_type=ParserData.MatchExact,
-                              preprocessor=self.add_live_channels,
-                              parser=self.episodeItemRegex, creator=self.create_episode_item)
+                              preprocessor=self.add_live_channels_and_folders)
+
+        # A-Z listing
+        self._add_data_parser("#alphalisting", preprocessor=self.alpha_listing)
+        self._add_data_parser("https://www.bbc.co.uk/iplayer/a-z/",
+                              preprocessor=self.extract_json, json=True,
+                              parser=["programmes"], creator=self.create_show_items)
+
+        # Video item listing
+        self._add_data_parser("*", preprocessor=self.redirect_to_main_show)
+        # self._add_data_parser("*", json=True,
+        #                       name="Parsing of the main video item of the listing.",
+        #                       parser=[], creator=self.create_video_item)
+        self._add_data_parser("*", json=True,
+                              name="Parsing of the related video items.",
+                              parser=["entities"], creator=self.create_video_item)
 
         # Standard items
-        self._add_data_parser("*", preprocessor=self.pre_process_folder_list)
-        self.folderItemRegex = r'<a href="(?<url>/iplayer/brand/[^"]+)"[^>]*>\W+<i[^>]+></i>\W+' \
-                               r'<span[^>]+>(?<title>[^<]+)<'.replace("(?<", "(?P<")
-        self._add_data_parser("*", parser=self.folderItemRegex, creator=self.create_folder_item)
-        self.videoItemRegex = r'<a\W+href="/iplayer/episode/(?<url>[^/]+)[^>]+>\W+<div[^>]+>[^>]+' \
-                              r'</div>\W+(?:<div[^>]+>[^>]+</div>\W+)?[\w\W]{0,500}?<source ' \
-                              r'srcset="(?<thumburl>[^"]+)"[\w\W]{0,500}?<div class="secondary">' \
-                              r'\W+<div[^>]+>(?<title>[^<]+)</div>\W+(?:<div[^>]+>(?<subtitle>' \
-                              r'[^<]+)</div>\W+)?<p[^>]*>(?<description>[^<]*)</p>[\w\W]{0,1000}?' \
-                              r'(?:<span class="release">\W+First shown: (?<day>\d+) (?<month>\w+) ' \
-                              r'(?<year>\d+)|<div class="period")'
-        self.videoItemRegex = Regexer.from_expresso(self.videoItemRegex)
-        self._add_data_parser("*", parser=self.videoItemRegex, creator=self.create_video_item)
+        # self._add_data_parser("*", preprocessor=self.pre_process_folder_list)
+        # self.folderItemRegex = r'<a href="(?<url>/iplayer/brand/[^"]+)"[^>]*>\W+<i[^>]+></i>\W+' \
+        #                        r'<span[^>]+>(?<title>[^<]+)<'.replace("(?<", "(?P<")
+        # self._add_data_parser("*", parser=self.folderItemRegex, creator=self.create_folder_item)
+        # self.videoItemRegex = r'<a\W+href="/iplayer/episode/(?<url>[^/]+)[^>]+>\W+<div[^>]+>[^>]+' \
+        #                       r'</div>\W+(?:<div[^>]+>[^>]+</div>\W+)?[\w\W]{0,500}?<source ' \
+        #                       r'srcset="(?<thumburl>[^"]+)"[\w\W]{0,500}?<div class="secondary">' \
+        #                       r'\W+<div[^>]+>(?<title>[^<]+)</div>\W+(?:<div[^>]+>(?<subtitle>' \
+        #                       r'[^<]+)</div>\W+)?<p[^>]*>(?<description>[^<]*)</p>[\w\W]{0,1000}?' \
+        #                       r'(?:<span class="release">\W+First shown: (?<day>\d+) (?<month>\w+) ' \
+        #                       r'(?<year>\d+)|<div class="period")'
+        # self.videoItemRegex = Regexer.from_expresso(self.videoItemRegex)
+        # self._add_data_parser("*", parser=self.videoItemRegex, creator=self.create_video_item)
 
         # Live channels
         self._add_data_parser("http://vs-hds-uk-live.edgesuite.net/",
@@ -80,7 +96,7 @@ class Channel(chn_class.Channel):
         # ====================================== Actual channel setup STOPS here =======================================
         return
 
-    def create_episode_item(self, result_set):
+    def create_episode_item(self, result_set, append_subtitle=True):
         """ Creates a new MediaItem for an episode.
 
         This method creates a new MediaItem from the Regular Expression or Json
@@ -88,6 +104,7 @@ class Channel(chn_class.Channel):
         and are specific to the channel.
 
         :param list[str]|dict[str,str] result_set: The result_set of the self.episodeItemRegex
+        :param bool append_subtitle:               Should we append a subtitle?
 
         :return: A new MediaItem of type 'folder'.
         :rtype: MediaItem|None
@@ -95,9 +112,23 @@ class Channel(chn_class.Channel):
         """
 
         Logger.trace(result_set)
-        item = chn_class.Channel.create_episode_item(self, result_set)
-        if item is not None:
-            item.name = "Shows: %s" % (item.name.upper(),)
+
+        url = result_set['href']
+        if not url.startswith("http"):
+            url = "{}{}".format(self.baseUrl, url)
+
+        title = result_set["title"]
+        if title is None:
+            title = self.parentItem.name
+        elif append_subtitle and "subtitle" in result_set:
+            title = "{} - {}".format(title, result_set["subtitle"])
+
+        item = MediaItem(title, url)
+        item.description = result_set.get('synopsis', item.description)
+
+        image_template = result_set.get("imageTemplate")
+        item.fanart = image_template.replace("{recipe}", "1920x1080")
+        item.thumb = image_template.replace("{recipe}", "608x342")
         return item
 
     def pre_process_folder_list(self, data):
@@ -171,16 +202,8 @@ class Channel(chn_class.Channel):
 
         """
 
-        if "subtitle" in result_set and not result_set["subtitle"]:
-            del result_set["subtitle"]
-
-        item = chn_class.Channel.create_video_item(self, result_set)
-        vid = item.url.replace(self.baseUrl, "")
-        item.url = "http://www.bbc.co.uk/iplayer/episode/%s" % (vid,)
-        if "year" in result_set and result_set["year"]:
-            month = DateHelper.get_month_from_name(result_set["month"], "en", short=True)
-            item.set_date(result_set["year"], month, result_set["day"])
-
+        item = self.create_episode_item(result_set["props"], append_subtitle=False)
+        item.type = "video"
         item.isGeoLocked = True
         return item
 
@@ -208,16 +231,10 @@ class Channel(chn_class.Channel):
 
         Logger.debug('Starting update_video_item for %s (%s)', item.name, self.channelName)
 
-        Logger.trace(item.url)
-        if not item.url.startswith("http://www.bbc.co.uk/mediaselector/"):
-            Logger.debug("Determining the stream URL")
-            data = UriHandler.open(item.url, proxy=self.proxy)
-            needle = r'"vpid"\W*"([^"]+)"'
-            vid = Regexer.do_regex(needle, data)[-1]
-            stream_data_url = "http://open.live.bbc.co.uk/mediaselector/5/select/version/" \
-                              "2.0/mediaset/iptv-all/vpid/%s" % (vid,)
-        else:
-            stream_data_url = item.url
+        data = UriHandler.open(item.url, proxy=self.proxy)
+        json_data, _ = self.extract_json(data)
+        video_id = json_data.get_value("versions", 0, "id")
+        stream_data_url = "http://open.live.bbc.co.uk/mediaselector/5/select/version/2.0/mediaset/iptv-all/vpid/{}".format(video_id)
 
         # this URL is one from the webbrowser but requires a security part. So NOT:
         # streamDataUrl = "http://open.live.bbc.co.uk/mediaselector/5/select/version
@@ -240,11 +257,19 @@ class Channel(chn_class.Channel):
             r'(<connection[^>]+>\W*)'
             r'(<connection[^>]+>\W*)?'
             r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
+            r'(<connection[^>]+>\W*)?'
             r'(<connection[^>]+>\W*)?</media>', stream_data)
 
         for connection_data in connection_datas:
             # first the bitrate
-            bitrate = connection_data[0]
+            bitrate = int(connection_data[0])
             Logger.trace("Found Media: %s", connection_data)
 
             # go through the available connections
@@ -253,6 +278,7 @@ class Channel(chn_class.Channel):
                     continue
 
                 connection_xml = XmlHelper(connection)
+                stream_bitrate = bitrate
                 Logger.trace("Analyzing Connection: %s", connection)
                 supplier = connection_xml.get_tag_attribute("connection", {"supplier": None})
                 protocol = connection_xml.get_tag_attribute("connection", {"protocol": None})
@@ -265,13 +291,16 @@ class Channel(chn_class.Channel):
                              protocol, transfer_format, supplier, bitrate)
 
                 if protocol.startswith("http"):
-                    if transfer_format != "hls":
+                    if transfer_format != "hls":  # and transfer_format != "dash":
                         Logger.debug("Ignoring TransferFormat: %s", transfer_format)
                         continue
                     if "lime" in supplier or "mf_akamai_uk" in supplier:
-                        Logger.debug("Ignoring Supplier: %s", supplier)
-                        continue
+                        # Prefer others
+                        stream_bitrate -= 1
+                        # Logger.debug("Ignoring Supplier: %s", supplier)
+                        # continue
                     url = connection_xml.get_tag_attribute("connection", {"href": None})
+
                 elif protocol.startswith("rtmp"):
                     Logger.warning("Ignoring RTMP for now")
                     continue
@@ -279,7 +308,11 @@ class Channel(chn_class.Channel):
                     Logger.warning("Unknown protocol: %s", protocol)
                     continue
 
-                part.append_media_stream(url, bitrate)
+                if transfer_format == "hls":
+                    item.complete = M3u8.update_part_with_m3u8_streams(part, url, proxy=self.proxy, bitrate=stream_bitrate)
+                elif transfer_format == "dash":
+                    strm = part.append_media_stream(url, bitrate)
+                    Mpd.set_input_stream_addon_input(strm, self.proxy)
 
         # get the subtitle
         subtitles = Regexer.do_regex(
@@ -295,7 +328,7 @@ class Channel(chn_class.Channel):
         Logger.trace('finishing update_video_item: %s.', item)
         return item
 
-    def add_live_channels(self, data):
+    def add_live_channels_and_folders(self, data):
         """ Performs pre-process actions for data processing.
 
         Accepts an data from the process_folder_list method, BEFORE the items are
@@ -313,6 +346,7 @@ class Channel(chn_class.Channel):
         """
 
         Logger.info("Generating Live channels")
+        items = []
 
         live_channels = [
             {"name": "BBC 1 HD", "code": "bbc_one_hd", "image": "bbc1large.png"},
@@ -338,6 +372,7 @@ class Channel(chn_class.Channel):
         live = MediaItem("Live Channels", "")
         live.dontGroup = True
         live.type = "folder"
+        items.append(live)
 
         for channel in live_channels:
             url = "http://a.files.bbci.co.uk/media/live/manifesto/audio_video/simulcast/hds/uk/pc/ak/%(code)s.f4m" % channel
@@ -349,7 +384,109 @@ class Channel(chn_class.Channel):
             item.thumb = self.get_image_location(channel["image"])
             live.items.append(item)
 
-        return data, [live, ]
+        extra = MediaItem("Shows (A-Z)", "#alphalisting")
+        extra.complete = True
+        extra.icon = self.icon
+        extra.thumb = self.noImage
+        extra.description = "Alphabetical show listing of BBC shows"
+        extra.dontGroup = True
+        extra.set_date(2200, 1, 1, text="")
+        items.append(extra)
+
+        return data, items
+
+    def alpha_listing(self, data):
+        """ Creates a alpha listing with items pointing to the alpha listing on line.
+
+        :param str data: The retrieve data that was loaded for the current item and URL.
+
+        :return: A tuple of the data and a list of MediaItems that were generated.
+        :rtype: tuple[str|JsonHelper,list[MediaItem]]
+
+        """
+
+        Logger.info("Generating an Alpha list for BBC")
+
+        items = []
+        # https://www.bbc.co.uk/iplayer/a-z/a
+
+        title_format = LanguageHelper.get_localized_string(LanguageHelper.StartWith)
+        url_format = "https://www.bbc.co.uk/iplayer/a-z/%s"
+        for char in "abcdefghijklmnopqrstuvwxyz0":
+            if char == "0":
+                char = "0-9"
+            sub_item = MediaItem(title_format % (char.upper(),), url_format % (char,))
+            sub_item.complete = True
+            sub_item.icon = self.icon
+            sub_item.thumb = self.noImage
+            sub_item.dontGroup = True
+            sub_item.HttpHeaders = {"X-Requested-With": "XMLHttpRequest"}
+            items.append(sub_item)
+        return data, items
+
+    def redirect_to_main_show(self, data):
+        """ Determines the actual URL of a show and fetches it's videos
+
+        :param str data: The retrieve data that was loaded for the current item and URL.
+
+        :return: A tuple of the data and a list of MediaItems that were generated.
+        :rtype: tuple[JsonHelper,list[MediaItem]]
+
+        """
+
+        json_data, items = self.extract_json(data)
+
+        # 325	200	HTTPS	www.bbc.co.uk	/iplayer/episode/b0404c5j/absolute-genius-with-dick-and-dom-series-2-10-talbot	65.993	no-store, must-revalidate, max-age=0	text/html; charset=utf-8	kodi:14240
+        # 326	200	HTTP	Tunnel to	www.bbc.co.uk:443	914			kodi:14240
+        # 327	301	HTTPS	www.bbc.co.uk	/iplayer/episodes/b03srr0b	94	no-store, must-revalidate, max-age=0	text/plain; charset=utf-8	kodi:14240
+        # 328	200	HTTPS	www.bbc.co.uk	/iplayer/episodes/b03srr0b/absolute-genius-with-dick-and-dom	60.571	no-store, must-revalidate, max-age=0	text/html; charset=utf-8	kodi:14240
+        show_id = json_data.get_value("episode", "tleoId")
+        url = "{}/iplayer/episodes/{}".format(self.baseUrl, show_id)
+        data = UriHandler.open(url, proxy=self.proxy)
+        json_data, items = self.extract_json(data)
+        return json_data, items
+
+    def extract_json(self, data):
+        """ Extracts the JSON datda from a main list.
+
+        :param str data: The retrieve data that was loaded for the current item and URL.
+
+        :return: A tuple of the data and a list of MediaItems that were generated.
+        :rtype: tuple[JsonHelper,list[MediaItem]]
+
+        """
+
+        items = []
+        json_regex = r'window.__IPLAYER_REDUX_STATE__ = (.*?);\s*</script>'
+        json_data = Regexer.do_regex(json_regex, data)
+        return JsonHelper(json_data[0]), items
+
+    def create_show_items(self, result_set):
+        """ Creates a new MediaItem for an episode.
+
+        This method creates a new MediaItem from the Regular Expression or Json
+        results <result_set>. The method should be implemented by derived classes
+        and are specific to the channel.
+
+        :param dict[str,dict]] result_set: The result_set of the self.episodeItemRegex
+
+        :return: A new MediaItem of type 'folder'.
+        :rtype: MediaItem|None
+
+        """
+
+        items = []
+        for char, data in result_set.items():
+            shows = data.get("entities")
+            if not shows:
+                continue
+
+            for item_data in shows:
+                item = self.create_episode_item(item_data["props"])
+                if items is not None:
+                    items.append(item)
+
+        return items or None
 
     def update_live_item(self, item):
         """ Updates an existing MediaItem with more data.
