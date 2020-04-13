b9c10af785cbd52699dd532d8ffa748adfac6d8e
Bas Rieter <basrieter@gmail.com>
2020-04-13 10:24:54 +0200

    Added: translation strings asking for a newer Kodi version

--- resources/lib/helpers/languagehelper.py
+++ resources/lib/helpers/languagehelper.py
@@ -35,6 +35,8 @@ class LanguageHelper(object):
     Disabled = 30074
     Retrospect = 30008
     MissingAddon = 30015
+    UpdateRequired = 30044
+    UpdateToNewKodi = 30045
 
     Monday = 30190
     Tuesday = 30191
