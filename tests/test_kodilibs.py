fd65e86f8478baf4683ded3defacfcbab2a5ed19
Bas Rieter <basrieter@gmail.com>
2020-04-15 22:31:39 +0200

    Added: unittest to make sure the testing stubs are OK

---
+++ tests/test_kodilibs.py
@@ -0,0 +1,15 @@
+import unittest
+
+
+class TestKodiLibs(unittest.TestCase):
+    def test_calling_id(self):
+        import xbmcaddon
+        a = xbmcaddon.Addon()
+        add_on_id = a.getAddonInfo("id")
+        self.assertEqual("plugin.video.retrospect", add_on_id)
+
+    def test_other_id(self):
+        import xbmcaddon
+        a = xbmcaddon.Addon("plugin.video.youtube")
+        add_on_id = a.getAddonInfo("id")
+        self.assertEqual("plugin.video.youtube", add_on_id)
