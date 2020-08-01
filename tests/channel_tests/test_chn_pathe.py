a4660ddb0f345ca41f42e2b2734b25ff760a287a
Bas Rieter <basrieter@gmail.com>
2020-08-01 17:30:41 +0200

    Changed: Disable Pathe tests from GitHub as they time-out

--- tests/channel_tests/test_chn_pathe.py
+++ tests/channel_tests/test_chn_pathe.py
@@ -1,4 +1,6 @@
 # SPDX-License-Identifier: CC-BY-NC-SA-4.0
+import os
+import unittest
 import datetime
 
 from tests.channel_tests.channeltest import ChannelTest
@@ -12,10 +14,12 @@ class TestPatheChannel(ChannelTest):
     def test_channel_exists(self):
         self.assertIsNotNone(self.channel)
 
+    @unittest.skipIf("CI" in os.environ, "Skipping in CI due to time-outs via GitHub")
     def test_main_list(self):
         items = self.channel.process_folder_list(None)
         self.assertGreaterEqual(len(items), 10, "No items found in mainlist")
 
+    @unittest.skipIf("CI" in os.environ, "Skipping in CI due to time-outs via GitHub")
     def test_tomorrows_trailers(self):
         today = datetime.datetime.now()
         tomorrow = today + datetime.timedelta(days=1)
@@ -23,6 +27,7 @@ class TestPatheChannel(ChannelTest):
             tomorrow.year, tomorrow.month, tomorrow.day)
         self._test_folder_url(url, expected_results=10)
 
+    @unittest.skipIf("CI" in os.environ, "Skipping in CI due to time-outs via GitHub")
     def test_now_playing(self):
         url = "https://connect.pathe.nl/v1/cinemas/7/movies/nowplaying"
         self._test_folder_url(url, expected_results=10)
