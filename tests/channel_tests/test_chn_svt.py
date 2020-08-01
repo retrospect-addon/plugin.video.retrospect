85da2c90a2bdcd996b6fb3beae6bd6df26e6a0c3
Bas Rieter <basrieter@gmail.com>
2020-08-01 12:23:14 +0200

    Added: 'Latest news' option to SVT

--- tests/channel_tests/test_chn_svt.py
+++ tests/channel_tests/test_chn_svt.py
@@ -13,7 +13,7 @@ class TestSvtChannel(ChannelTest):
 
     def test_main_list(self):
         items = self.channel.process_folder_list(None)
-        self.assertEqual(len(items), 10, "No items found in mainlist")
+        self.assertEqual(len(items), 11, "No items found in mainlist")
 
     def test_main_program_list(self):
         self._test_folder_url(
@@ -28,3 +28,10 @@ class TestSvtChannel(ChannelTest):
             "Name=GridPage&variables=%7B%22selectionId%22%3A%20%22popular%22%7D&extensions=%7B%22"
             "persistedQuery%22%3A%20%7B%22version%22%3A%201%2C%20%22sha256Hash%22%3A%20%22265677"
             "a2465d93d39b536545cdc3664d97e3843ce5e34f145b2a45813b85007b%22%7D%7D", 5)
+
+    def test_recent_news(self):
+        url = "https://api.svt.se/contento/graphql?ua=svtplaywebb-play-render-prod-client&" \
+              "operationName=GenreLists&variables=%7B%22genre%22%3A%20%5B%22nyheter%22%5D%7D&" \
+              "extensions=%7B%22persistedQuery%22%3A%20%7B%22version%22%3A%201%2C%20%22sha256Hash" \
+              "%22%3A%20%2290dca0b51b57904ccc59a418332e43e17db21c93a2346d1c73e05583a9aa598c%22%7D%7D"
+        self._test_folder_url(url, expected_results=4)
