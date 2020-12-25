ace9a6a1fbc6149d47be03017a348d51eba428ff
Bas Rieter <basrieter@gmail.com>
2020-12-25 18:22:55 +0100

    Added: kodivfs.File wrapper class for xbmcvfs.File
    Allows the use for `with KodiVfsFile() as fp:` and makes it more like Kodi Matrix syntax (See https://romanvm.github.io/Kodistubs/_autosummary/xbmcvfs.html#xbmcvfs.File)

---
+++ resources/lib/helpers/kodivfs.py
@@ -0,0 +1,32 @@
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+import xbmcvfs
+
+
+class File(object):
+    def __init__(self, path, flags="r"):
+        """ Wrapper for xbmcvfs.File class with __enter__() and __exit__()
+
+        :param str path:    The file or directory to open
+        :param str flags:   The flags used to open the file
+
+        """
+
+        self.flags = flags
+        self.path = path
+
+        self.__file = None
+
+    def __enter__(self):
+        """ Enter the File block
+
+        :rtype: xbmcvfs.File
+
+        """
+
+        self.__file = xbmcvfs.File(self.path, self.flags)
+        return self.__file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """ Exiting """
+        self.__file.close()
