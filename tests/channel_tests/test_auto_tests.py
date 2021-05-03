dcb3bbd9e82932d2c1475b49137ca4b83b792980
Bas Rieter <basrieter@gmail.com>
2021-05-03 23:09:11 +0200

    Updated: Also check sublistings.

--- tests/channel_tests/test_auto_tests.py
+++ tests/channel_tests/test_auto_tests.py
@@ -27,19 +27,10 @@ class AutoChannelTest(unittest.TestCase):
 
     def test_main_lists(self):
         from resources.lib.chn_class import Channel
-        from resources.lib.channelinfo import ChannelInfo
-        channels_tested = os.listdir(os.path.join("tests", "channel_tests"))
-        channels_tested = [chn[5:-3] for chn in channels_tested if chn.startswith("test_")]
-        channels_tested.append("chn_vtmbe")
         failed = []
+        failed_subs = []
 
-        for channel_info in self.channels:      # type: ChannelInfo
-            if channel_info.moduleName in channels_tested:
-                continue
-
-            if channel_info.uses_external_addon:
-                continue
-
+        for channel_info in self.__get_channels_to_test():
             channel = channel_info.get_channel()  # type: Channel
             try:
                 items = channel.process_folder_list()
@@ -48,7 +39,34 @@ class AutoChannelTest(unittest.TestCase):
 
             if len(items) < 3:
                 failed.append(channel_info)
+                continue
+
+            item = items[-1]
+            try:
+                sub_items = channel.process_folder_list(item)
+            except:
+                sub_items = []
+
+            if len(sub_items) == 0:
+                failed_subs.append(channel_info)
 
         self.assertEqual(
-            len(failed), 0,
-            msg="Mainlist failed for for:\n - {}".format("\n - ".join([str(f) for f in failed])))
+            len(failed + failed_subs), 0,
+            msg="Mainlist failed for for:\n - {}\nSublisting failed for for:\n - {}".format(
+                "\n - ".join([str(f) for f in failed]),
+                "\n - ".join([str(f) for f in failed_subs])))
+
+    def __get_channels_to_test(self):
+        from resources.lib.channelinfo import ChannelInfo
+        channels_tested = os.listdir(os.path.join("tests", "channel_tests"))
+        channels_tested = [chn[5:-3] for chn in channels_tested if chn.startswith("test_")]
+        channels_tested.append("chn_vtmbe")
+
+        for channel_info in self.channels:  # type: ChannelInfo
+            if channel_info.moduleName in channels_tested:
+                continue
+
+            if channel_info.uses_external_addon:
+                continue
+
+            yield channel_info
