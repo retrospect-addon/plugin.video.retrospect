691cbe1934e25dc1c8f25ccb06b8d40bc158554e
Bas Rieter <basrieter@gmail.com>
2021-10-23 20:11:40 +0200

    Fixed: MTV.nl and MTV.de broken.

--- tests/channel_tests/test_chn_mtvde.py
+++ tests/channel_tests/test_chn_mtvde.py
@@ -6,15 +6,25 @@ from . channeltest import ChannelTest
 class TestMtvDeChannel(ChannelTest):
     # noinspection PyPep8Naming
     def __init__(self, methodName):  # NOSONAR
-        super(TestMtvDeChannel, self).__init__(methodName, "channel.mtv.mtvnl", "mtvde")
+        super(TestMtvDeChannel, self).__init__(methodName, "channel.nick.nickelodeon", "mtvde")
 
-    def test_channel_mtv_exists(self):
+    def test_channel_mtvde_exists(self):
         self.assertIsNotNone(self.channel)
 
-    def test_channel_mtv_main_list(self):
+    def test_channel_mtvde_main_list(self):
         items = self.channel.process_folder_list(None)
         self.assertGreater(len(items), 25)
 
     def test_video_list(self):
-        url = "http://www.mtv.de/feeds/intl_m112/V8_0_0/62701278-3c80-4ef6-a801-1df8ffca7c78/90a54067-9c44-434f-a063-c06232cf047d"
+        url = "https://www.mtv.de/serien/jersey-shore-family-vacation"
         self._test_folder_url(url, expected_results=4)
+
+    def test_video(self):
+        url = "https://www.mtv.de/folgen/qbpkgz/jersey-shore-family-vacation-snooki-geht-nach-washington-teil-eins-staffel-3-ep-13"
+        self._test_video_url(url)
+
+    def test_list_with_seasons(self):
+        url = "https://www.mtv.de/serien/jersey-shore-family-vacation"
+        items = self._test_folder_url(url, expected_results=4)
+        folders = [i for i in items if i.media_type == "season"]
+        self.assertGreater(len(folders), 0)
