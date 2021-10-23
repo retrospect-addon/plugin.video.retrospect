22893e453943753bf530e97634a363ad438dd437
Bas Rieter <basrieter@gmail.com>
2021-10-23 16:24:07 +0200

    Fixed: UR Play video unit test.

--- tests/channel_tests/test_chn_urplay.py
+++ tests/channel_tests/test_chn_urplay.py
@@ -9,7 +9,7 @@ class TestUrPlayChannel(ChannelTest):
         super(TestUrPlayChannel, self).__init__(methodName, "channel.se.urplay", None)
 
     def test_video_play(self):
-        url = "https://urplay.se/program/218054-lilla-aktuellt-skola-2020-10-16"
+        url = "https://urplay.se/serie/189896-aarons-nya-land"
         self._test_video_url(url)
 
     def test_video_audio(self):
