38a2568a8e2fa9fa771e39265280713c2983c69a
Bas Rieter <basrieter@gmail.com>
2021-11-25 00:20:10 +0100

    Fixed: RTV Utrecht moved to Regio Groei platform.

--- tests/channel_tests/test_chn_rpoapp.py
+++ tests/channel_tests/test_chn_rpoapp.py
@@ -8,25 +8,6 @@ class TestRpoAppChannel(ChannelTest):
     def __init__(self, methodName):  # NOSONAR
         super(TestRpoAppChannel, self).__init__(methodName, "channel.regionalnl.rpoapp", "rtvutrecht")
 
-    def test_channel_exists_utrecht(self):
-        self.assertIsNotNone(self.channel)
-
-    def test_main_list_utrecht(self):
-        items = self.channel.process_folder_list(None)
-        self.assertGreaterEqual(len(items), 20, "No items found in mainlist")
-
-    def test_video_listing_utrecht(self):
-        url = "https://www.rtvutrecht.nl/gemist/uitzending/rtvutrecht/unieuws/"
-        self._test_folder_url(url, expected_results=20)
-
-    def test_video_utrecht(self):
-        url = "https://www.rtvutrecht.nl/gemist/uitzending/rtvutrecht/unieuws/20211119-1700/"
-        self._test_video_url(url)
-
-    def test_video_utrecht_javascript(self):
-        url = "https://www.rtvutrecht.nl/gemist/uitzending/rtvutrecht/unieuws/20211115-1700/"
-        self._test_video_url(url)
-
     def test_channel_exists_zeeland(self):
         channel = self._switch_channel("omroepzeeland")
         self.assertIsNotNone(channel)
