c7b217e0c9f2d1baea2ba2ac687bcbd76332926c
Mats Arvidsson <46671821+matsarv@users.noreply.github.com>
2021-12-14 11:57:06 +0100

    Update: Replace is_episodic with season/episode

--- channels/channel.mtg/viafree/chn_viafree.py
+++ channels/channel.mtg/viafree/chn_viafree.py
@@ -464,7 +464,8 @@ class Channel(chn_class.Channel):
         Logger.trace(result_set)
 
         drm_locked = False
-        is_episodic = False
+        season = 0
+        episode = 0
         geo_blocked = result_set["is_geo_blocked"]
 
         title = result_set["title"]
@@ -496,7 +497,6 @@ class Channel(chn_class.Channel):
 
             # if the name had the episode in it, translate it
             if episode > 0 and season > 0:  # and not webisode:
-                is_episodic = True
                 title = "{} {}".format(LanguageHelper.get_localized_string(LanguageHelper.EpisodeId), episode)
             else:
                 Logger.debug("Found episode '0' or websido '%s': using name instead of episode number", title)
@@ -536,8 +536,8 @@ class Channel(chn_class.Channel):
             item.set_date(date_info[0], date_info[1], date_info[2], time_info[0], time_info[1], 0)
 
         item.media_type = mediatype.EPISODE
-        if is_episodic:
-            item.set_season_info(season, episode)        
+        if episode > 0 and season > 0:
+            item.set_season_info(season, episode)
         item.complete = False
         item.isGeoLocked = geo_blocked
         item.isDrmProtected = drm_locked
