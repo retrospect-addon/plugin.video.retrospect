2aa6d45ba6c1eb9b6ac325769c6c82631a3bab7e
Bas Rieter <basrieter@gmail.com>
2021-12-29 14:05:43 +0100

    Fixed: Better determine the stream url for RegioGroei.

--- tests/channel_tests/test_chn_regiogroei.py
+++ tests/channel_tests/test_chn_regiogroei.py
@@ -9,26 +9,30 @@ class TestRegioGroei(ChannelTest):
     def __init__(self, methodName):  # NOSONAR
         super(TestRegioGroei, self).__init__(methodName, "channel.regionalnl.regiogroei", "rtvutrecht")
 
-    def test_channel_exists_utrecht(self):
+    def test_rtv_utrecht_channel_exists(self):
         self.assertIsNotNone(self.channel)
 
-    def test_main_list_utrecht(self):
+    def test_rtv_utrecht_main_list(self):
         items = self.channel.process_folder_list(None)
         self.assertGreaterEqual(len(items), 20, "No items found in mainlist")
 
-    def test_video_list(self):
+    def test_rtv_utrecht_video_list(self):
         url = "https://api.regiogroei.cloud/page/program/RTVU_2190407?slug=binnenstebuiten&origin=RTVU_2190407"
         self._test_folder_url(url, expected_results=2)
 
-    def test_video(self):
-        url = "https://rtvutrecht.bbvms.com/p/regiogroei_utrecht_web_videoplayer/c/4456927.json"
+    def test_rtv_utrecht_video(self):
+        url = "https://api.regiogroei.cloud/page/episode/RTVU_2190428"
         self._test_video_url(url)
 
-    def test_video_auto_birate(self):
-        url = "https://rtvutrecht.bbvms.com/p/regiogroei_utrecht_web_videoplayer/c/4464173.json"
-        self._test_video_url(url)
+    def test_rtv_utrecht_video_auto_birate(self):
+        url = "https://api.regiogroei.cloud/page/episode/RTVU_2213398"
+        item = self._test_video_url(url)
 
-    def test_day_utrecht(self):
+        item.streams.sort(key=lambda s: s.Bitrate)
+        highest = item.streams[-1]
+        self.assertTrue("m3u8" in highest.Url)
+
+    def test_rtv_utrecht_day(self):
         today = datetime.datetime.now() - datetime.timedelta(days=1)
         tomorrow = today + datetime.timedelta(days=1)
         url = "https://api.regiogroei.cloud/programs/rtv-utrecht?startDate=" \
@@ -36,41 +40,36 @@ class TestRegioGroei(ChannelTest):
             format(today.year, today.month, today.day, tomorrow.year, tomorrow.month, tomorrow.day)
         self._test_folder_url(url, expected_results=2)
 
-    def test_live_utrecht(self):
-        url = "https://rtvutrecht.bbvms.com/p/regiogroei_utrecht_web_videoplayer/c/3742011.json"
+    def test_rtv_utrecht_live(self):
+        url = "https://api.regiogroei.cloud/page/channel/rtv-utrecht?channel=rtv-utrecht"
         self._test_video_url(url)
 
     # Zeeland stuff
-    def test_channel_exists_zeeland(self):
+    def test_zeeland_channel_exists(self):
         channel = self._switch_channel("omroepzeeland")
         self.assertIsNotNone(channel)
 
-    def test_main_list_zeeland(self):
+    def test_zeeland_main_list(self):
         self._switch_channel("omroepzeeland")
         items = self.channel.process_folder_list(None)
         self.assertGreater(len(items), 25)
 
-    def test_video_listing_zeeland(self):
+    def test_zeeland_video_listing(self):
         self._switch_channel("omroepzeeland")
-        items = self.channel.process_folder_list(None)
-        self.assertGreaterEqual(len(items), 20, "No items found in mainlist")
+        url = "https://api.regiogroei.cloud/page/program/370060057?slug=binnenstebuiten&origin=370060057"
+        self._test_folder_url(url, expected_results=10)
 
-    def test_video_zeeland(self):
+    def test_zeeland_video(self):
         self._switch_channel("omroepzeeland")
-        url = "https://omroepzeeland.bbvms.com/p/regiogroei_zeeland_web_videoplayer/c/sourceid_string:370275114-Code-Groen-28-09-2021-ZNB210914DY.json"
+        url = "https://api.regiogroei.cloud/page/episode/370275114"
         self._test_video_url(url)
 
-    def test_video_zeeland_should_load_no_source_id(self):
+    def test_zeeland_live(self):
         self._switch_channel("omroepzeeland")
-        url = "https://omroepzeeland.bbvms.com/p/regiogroei_zeeland_web_videoplayer/c/sourceid_string:2430771.json"
+        url = "https://api.regiogroei.cloud/page/channel/omroep-zeeland-tv?channel=omroep-zeeland-tv"
         self._test_video_url(url)
 
-    def test_live_zeeland(self):
-        self._switch_channel("omroepzeeland")
-        url = "https://omroepzeeland.bbvms.com/p/regiogroei_zeeland_web_videoplayer/c/3745936.json"
-        self._test_video_url(url)
-
-    def test_day_zeeland(self):
+    def test_zeeland_day(self):
         self._switch_channel("omroepzeeland")
         today = datetime.datetime.now() - datetime.timedelta(days=1)
         tomorrow = today + datetime.timedelta(days=1)
@@ -79,3 +78,38 @@ class TestRegioGroei(ChannelTest):
             format(today.year, today.month, today.day, tomorrow.year, tomorrow.month, tomorrow.day)
         self._test_folder_url(url, expected_results=2)
 
+    # RTV Noord
+    def test_rtv_noord_channel_exists(self):
+        channel = self._switch_channel("rtvnoord")
+        self.assertIsNotNone(channel)
+
+    def test_rtv_noord_mainlist(self):
+        self._switch_channel("rtvnoord")
+
+    def test_rtv_noord_video_listing(self):
+        self._switch_channel("rtvnoord")
+        url = "https://api.regiogroei.cloud/page/program/10010?slug=noord-vandaag&origin=10010"
+        self._test_folder_url(url, expected_results=20)
+
+    def test_rtv_noord_video(self):
+        self._switch_channel("rtvnoord")
+        url = "https://api.regiogroei.cloud/page/episode/C4915464A4E0AD8CC12587820042AB5C"
+        self._test_video_url(url)
+
+    def test_rtv_noord_live(self):
+        self._switch_channel("rtvnoord")
+        url = "https://api.regiogroei.cloud/page/channel/tv-noord?channel=tv-noord"
+        self._test_video_url(url)
+
+        items = self.channel.process_folder_list(None)
+        self.assertGreater(len(items), 5)
+
+    def test_rtv_noord_day(self):
+        self._switch_channel("rtvnoord")
+        today = datetime.datetime.now() - datetime.timedelta(days=1)
+        tomorrow = today + datetime.timedelta(days=1)
+
+        url = "https://api.regiogroei.cloud/programs/tv-noord?startDate=" \
+              "{:04d}-{:02d}-{:02d}&endDate={:04d}-{:02d}-{:02d}". \
+            format(today.year, today.month, today.day, tomorrow.year, tomorrow.month, tomorrow.day)
+        self._test_folder_url(url, expected_results=2)
