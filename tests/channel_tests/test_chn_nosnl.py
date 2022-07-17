4ab1fa44ae77a44999c1f6d18eac4ab1a3ca6166
Bas Rieter <basrieter@gmail.com>
2022-07-17 21:46:51 +0200

    Removed: "Meest bekeken" from NOS channel.

--- tests/channel_tests/test_chn_nosnl.py
+++ tests/channel_tests/test_chn_nosnl.py
@@ -15,9 +15,9 @@ class TestNosChannel(ChannelTest):
         items = self.channel.process_folder_list(None)
         self.assertEqual(len(items), 5, "No items found in mainlist")
 
-    def test_most_viewed_items(self):
-        url = "https://api.nos.nl/mobile/videos/most-viewed/phone.json"
-        self._test_folder_url(url, expected_results=10)
+    # def test_most_viewed_items(self):
+    #     url = "https://api.nos.nl/mobile/videos/most-viewed/phone.json"
+    #     self._test_folder_url(url, expected_results=10)
 
     def test_video_update(self):
         url = "https://api.nos.nl/mobile/video/2380242/phone.json"
