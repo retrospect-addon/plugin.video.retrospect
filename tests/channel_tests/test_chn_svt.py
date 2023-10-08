025af0f98f17998652ebda5d4977c223b76b71ad
Bas Rieter <basrieter@gmail.com>
2023-10-08 21:46:27 +0200

    Fixed: SVT tests.

--- tests/channel_tests/test_chn_svt.py
+++ tests/channel_tests/test_chn_svt.py
@@ -88,7 +88,7 @@ class TestSvtChannel(ChannelTest):
         self._test_folder_url(url, expected_results=10)
 
     def test_api_video_update(self):
-        url = "https://api.svt.se/videoplayer-api/video/e2DDJzo"
+        url = "https://api.svt.se/videoplayer-api/video/K5RLm5g"
         item = self._test_video_url(url)
         self.assertIsNotNone(item)
 
