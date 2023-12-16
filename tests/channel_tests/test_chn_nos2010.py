85aea735499f59f56b802f39bda2b5ff12f1fc91
Bas Rieter <basrieter@gmail.com>
2023-12-16 21:43:27 +0100

    Fixed: NPO test.

--- tests/channel_tests/test_chn_nos2010.py
+++ tests/channel_tests/test_chn_nos2010.py
@@ -46,7 +46,7 @@ class TestNpoChannel(ChannelTest):
         self.assertGreater(len(items[0].items), 10)
 
     def test_series_without_season(self):
-        items = self._test_folder_url("https://npo.nl/start/api/domain/programs-by-season?guid=9d87c512-bf2d-4df9-8e33-e0792615ed50", 4)
+        items = self._test_folder_url("https://npo.nl/start/api/domain/programs-by-season?guid=9d87c512-bf2d-4df9-8e33-e0792615ed50", 3)
         self.assertTrue(all([i.is_playable for i in items]))
 
     def test_series_with_seasons_via_slug(self):
