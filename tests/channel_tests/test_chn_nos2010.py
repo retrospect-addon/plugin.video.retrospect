7cba4509e102d128100c1f01e7185880bd111dbd
Bas Rieter <basrieter@gmail.com>
2024-01-02 01:00:14 +0100

    Fixed: Some NPO tests are time dependent (which sucks).

--- tests/channel_tests/test_chn_nos2010.py
+++ tests/channel_tests/test_chn_nos2010.py
@@ -39,6 +39,7 @@ class TestNpoChannel(ChannelTest):
         items = self.channel.process_folder_list(item)
         self.assertGreaterEqual(len(items), 7)
 
+    @unittest.skipUnless(6 < datetime.datetime.utcnow().hour < 23, "No now broadcasts after 00:00")
     def test_recent_sub_items(self):
         from resources.lib.mediaitem import MediaItem
         item = MediaItem("recent", "#recentday")
