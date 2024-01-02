ded4b72a3323b2766e22f687e479273dba37a476
Bas Rieter <basrieter@gmail.com>
2024-01-02 23:55:36 +0100

    Added: Show folder listing.

--- channels/channel.videoland/videolandnl/chn_videolandnl.py
+++ channels/channel.videoland/videolandnl/chn_videolandnl.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 import datetime
-from typing import Union, List, Optional
+from typing import Union, List, Optional, Tuple
 
 import pytz
 
@@ -11,8 +11,8 @@ from resources.lib.authentication.gigyahandler import GigyaHandler
 from resources.lib.helpers.datehelper import DateHelper
 from resources.lib.helpers.jsonhelper import JsonHelper
 from resources.lib.helpers.languagehelper import LanguageHelper
-from resources.lib.logger import Logger
 from resources.lib.mediaitem import MediaItem, FolderItem
+from resources.lib.parserdata import ParserData
 from resources.lib.streams.mpd import Mpd
 from resources.lib.urihandler import UriHandler
 from resources.lib.xbmcwrapper import XbmcWrapper
@@ -37,14 +37,23 @@ class Channel(chn_class.Channel):
         # setup the urls
         self.mainListUri = "https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/alias/home/layout?nbPages=1"
         # https://pc.middleware.videoland.bedrock.tech/6play/v2/platforms/m6group_web/services/videoland/programs?limit=999&offset=0&csa=tot_18_jaar&with=rights
+        # https://pc.middleware.videoland.bedrock.tech/6play/v2/platforms/m6group_web/services/videoland/programs/first-letters?csa=tot_18_jaar
 
         self._add_data_parser(self.mainListUri, requires_logon=True, json=True,
                               name="Mainlist for Videoland",
-                              parser=["blocks"], creator=self.create_main_list_item)
+                              parser=["blocks"], creator=self.create_mainlist_item)
 
-        self._add_data_parser("https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/service/videoland_root/block/",
-                              name="Main folder processor", json=True, requires_logon=True,
-                              parser=["content", "items"], creator=self.create_episode_item)
+        self._add_data_parsers([r"^https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/service/videoland_root/block/",
+                                r"^https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/program/\d+/block/"],
+                               match_type=ParserData.MatchRegex,
+                               name="Main processor that create content items (folders/videos) from blocks", json=True, requires_logon=True,
+                               parser=["content", "items"], creator=self.create_content_item)
+
+        self._add_data_parser(r"https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/program/\d+/layout",
+                              match_type=ParserData.MatchRegex, json=True, requires_logon=True,
+                              name="Parser for the main folder of a show show/program.",
+                              preprocessor=self.extract_program_id,
+                              parser=["blocks"], creator=self.create_program_item)
 
         self._add_data_parser("https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/video/", requires_logon=True,
                               name="Video updater", json=True, updater=self.update_video_item)
@@ -59,11 +68,11 @@ class Channel(chn_class.Channel):
 
         #===============================================================================================================
         # non standard items
-        self.__ignore_cookie_law()
+        self.__program_id = None
         self.__pages = 10
         self.__timezone = pytz.timezone("Europe/Amsterdam")
 
-    def create_main_list_item(self, result_set: Union[str, dict]) -> Union[MediaItem, List[MediaItem], None]:
+    def create_mainlist_item(self, result_set: Union[str, dict]) -> Union[MediaItem, List[MediaItem], None]:
         if not result_set["title"]:
             return None
         title = result_set["title"].get("long", result_set["title"].get("short"))
@@ -74,35 +83,42 @@ class Channel(chn_class.Channel):
         item = FolderItem(title, url, content_type=contenttype.EPISODES if feature_id.startswith("videos") else contenttype.TVSHOWS)
         return item
 
-    def create_episode_item(self, result_set: Union[str, dict]) -> Union[MediaItem, List[MediaItem], None]:
+    def create_content_item(self, result_set: Union[str, dict]) -> Union[MediaItem, List[MediaItem], None]:
         result_set: dict = result_set["itemContent"]
 
         title = result_set["title"]
         extra_title = result_set.get("extraTitle")
-        if extra_title:
+        if extra_title and title:
             title = f"{title} - {extra_title}"
-        item_id = result_set["id"]
+        elif not title and extra_title:
+            title = extra_title
 
         # What type is it
         action_info = result_set.get("action", {})
         action = action_info.get("label", "").lower()
+        item_id = action_info["target"]["value_layout"]["id"]
         if "content" in action:
-            url = f"https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/program/{item_id}/layout?nbPages=2"
+            url = f"https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/program/{item_id}/layout?nbPages={self.__pages}"
             item = FolderItem(title, url, content_type=contenttype.TVSHOWS)
         else:
-            video_id = action_info["target"]["value_layout"]["id"]
-            url = f"https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/video/{video_id}/layout?nbPages=2"
+            url = f"https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/video/{item_id}/layout?nbPages=2"
             item = MediaItem(title, url, media_type=mediatype.EPISODE)
         item.isPaid = action == "abonneren"
 
-        if "image" in result_set:
-            for ratio, image_id in result_set["image"]["idsByRatio"].items():
+        def set_images(item: MediaItem, image_info: dict, set_fanart: bool, set_poster: bool):
+            for ratio, image_id in image_info["idsByRatio"].items():
                 image_url = f"https://images-fio.videoland.bedrock.tech/v2/images/{image_id}/raw"
                 if ratio == "16:9":
                     item.thumb = image_url
-                    item.fanart = image_url
-                elif ratio == "2:3":
+                    if set_fanart:
+                        item.fanart = image_url
+                elif ratio == "2:3" and set_poster:
                     item.poster = image_url
+
+        if "image" in result_set and item.is_playable:
+            set_images(item, result_set["image"], set_fanart=False, set_poster=False)
+        elif "secondaryImage" in result_set and not item.is_playable:
+            set_images(item, result_set["secondaryImage"], set_fanart=True, set_poster=True)
 
         time_value = result_set["highlight"]
         if time_value and "min" in time_value:
@@ -132,6 +148,28 @@ class Channel(chn_class.Channel):
                 month = DateHelper.get_month_from_name(month, language="nl", short=True)
                 year = 2000 + int(year)
                 item.set_date(year, month, day)
+
+        return item
+
+    def extract_program_id(self, data: str) -> Tuple[Union[str, JsonHelper], List[MediaItem]]:
+        json_data = JsonHelper(data)
+        self.__program_id = json_data.get_value("entity", "id", fallback=None)
+        return json_data, []
+
+    def create_program_item(self, result_set: dict) -> Union[MediaItem, List[MediaItem], None]:
+        if not result_set["title"]:
+            return None
+
+        title = result_set["title"].get("long", result_set["title"].get("short"))
+        page_id = result_set["id"]
+        url = f"https://layout.videoland.bedrock.tech/front/v1/rtlnl/m6group_web/main/token-web-4/program/{self.__program_id}/block/{page_id}?nbPages=10&page=1"
+        item = FolderItem(title, url, content_type=contenttype.EPISODES)
+
+        # Preload them, but this makes paging difficult.
+        # for sub_result in result_set["content"].get("items", []) or []:
+        #     sub_item = self.create_content_item(sub_result)
+        #     if sub_item:
+        #         item.items.append(sub_item)
 
         return item
 
@@ -166,15 +204,6 @@ class Channel(chn_class.Channel):
             #     item.complete = True
             #     M3u8.set_input_stream_addon_input(stream)
         return item
-
-    def __ignore_cookie_law(self):
-        """ Accepts the cookies from RTL channel in order to have the site available """
-
-        Logger.info("Setting the Cookie-Consent cookie for www.uitzendinggemist.nl")
-
-        # the rfc2109 parameters is not valid in Python 2.4 (Xbox), so we ommit it.
-        UriHandler.set_cookie(name='rtlcookieconsent', value='yes', domain='.www.rtl.nl')
-        return
 
     def log_on(self):
         """ Logs on to a website, using an url.
