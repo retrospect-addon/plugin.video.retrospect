9491464d606ded8e2246d2a89a3bf2868c70b2a2
Bas Rieter <basrieter@gmail.com>
2024-07-16 00:55:23 +0200

    Changed: Subactions instead of inheritance.

--- resources/lib/actions/searchaction.py
+++ resources/lib/actions/searchaction.py
@@ -7,18 +7,22 @@ import xbmcplugin
 from resources.lib import contenttype
 from resources.lib.actions import action, keyword
 from resources.lib.actions.actionparser import ActionParser
+from resources.lib.actions.addonaction import AddonAction
 from resources.lib.actions.folderaction import FolderAction
 from resources.lib.addonsettings import AddonSettings, LOCAL
 from resources.lib.chn_class import Channel
 from resources.lib.helpers.htmlentityhelper import HtmlEntityHelper
 from resources.lib.helpers.languagehelper import LanguageHelper
-from resources.lib.helpers.stopwatch import StopWatch
 from resources.lib.logger import Logger
 from resources.lib.mediaitem import FolderItem, MediaItem
 from resources.lib.xbmcwrapper import XbmcWrapper
 
 
-class SearchAction(FolderAction):
+class SearchAction(AddonAction):
+    __channel: Channel
+    __needle: Optional[str]
+    __media_item: MediaItem
+
     def __init__(self, parameter_parser: ActionParser, channel: Channel, needle: Optional[str]):
         """Wraps the channel.process_folder_list
 
@@ -28,24 +32,26 @@ class SearchAction(FolderAction):
 
         """
 
-        super().__init__(parameter_parser, channel)
+        super().__init__(parameter_parser)
 
-        self.needle = needle if needle is None else HtmlEntityHelper.url_decode(needle)
-        self.settings = AddonSettings.store(store_location=LOCAL)
-        Logger.debug(f"Searching for: {self.needle}")
+        self.__needle = needle if needle is None else HtmlEntityHelper.url_decode(needle)
+        self.__settings = AddonSettings.store(store_location=LOCAL)
+        self.__media_item = parameter_parser.media_item
+        self.__channel = channel
+        Logger.debug(f"Searching for: {self.__needle}")
 
     def execute(self):
         # read the item from the parameters
-        selected_item: MediaItem = self._media_item
+        selected_item: MediaItem = self.__media_item
 
         # determine the parent guid
-        parent_guid = self.parameter_parser.get_parent_guid(self._channel, selected_item)
+        parent_guid = self.parameter_parser.get_parent_guid(self.__channel, selected_item)
 
-        if self.needle is None:
+        if self.__needle is None:
             self.__generate_search_history(selected_item, parent_guid)
             return
 
-        elif not self.needle:
+        elif not self.__needle:
             # Search input
             needle = XbmcWrapper.show_key_board()
             if not needle:
@@ -53,40 +59,40 @@ class SearchAction(FolderAction):
                 return
 
             # noinspection PyTypeChecker
-            history: List[str] = self.settings.get_setting("search", self._channel, [])
+            history: List[str] = self.__settings.get_setting("search", self.__channel, [])
             history = list(set([needle] + history))
-            self.settings.set_setting("search", history[0:10], self._channel)
+            self.__settings.set_setting("search", history[0:10], self.__channel)
 
             # Make sure we actually load a new URL so a refresh won't pop up a loading screen.
             needle = HtmlEntityHelper.url_encode(needle)
             xbmcplugin.endOfDirectory(self.handle, True, cacheToDisc=True)
-            url = self.parameter_parser.create_action_url(self._channel, action.SEARCH, needle=needle)
+            url = self.parameter_parser.create_action_url(self.__channel, action.SEARCH, needle=needle)
             xbmc.executebuiltin(f"Container.Update({url})")
 
         else:
-            watcher = StopWatch("Plugin processing Search Request", Logger.instance())
-            media_items = self._channel.search_site(needle=self.needle)
-            self._generate_kodi_items(media_items, parent_guid, selected_item, watcher)
+            media_items = self.__channel.search_site(needle=self.__needle)
+            folder_action = FolderAction(self.parameter_parser, self.__channel, items=media_items)
+            folder_action.execute()
 
     def __generate_search_history(self, selected_item: MediaItem, parent_guid: str):
-        watcher = StopWatch("Plugin processing Search History", Logger.instance())
-
         # noinspection PyTypeChecker
-        history: List[str] = self.settings.get_setting("search", self._channel, [])
+        history: List[str] = self.__settings.get_setting("search", self.__channel, [])
 
         media_items = []
         search_item = FolderItem(
             f"\b{LanguageHelper.get_localized_string(LanguageHelper.NewSearch)}",
-            f"{self._channel.search_url}&{keyword.NEEDLE}=",
+            f"{self.__channel.search_url}&{keyword.NEEDLE}=",
             content_type=contenttype.VIDEOS
         )
         media_items.append(search_item)
 
         for needle in history:
             encoded_needle = HtmlEntityHelper.url_encode(needle)
-            url = self.parameter_parser.create_action_url(self._channel, action.SEARCH,
+            url = self.parameter_parser.create_action_url(self.__channel, action.SEARCH,
                                                           needle=encoded_needle)
             item = FolderItem(needle, url, content_type=contenttype.VIDEOS)
             media_items.append(item)
 
-        self._generate_kodi_items(media_items, parent_guid, selected_item, watcher)
+        folder_action = FolderAction(self.parameter_parser, self.__channel, items=media_items)
+        folder_action.execute()
+
